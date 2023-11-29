d258aa9f00d74b1388a3817ffd88c72196257aed
Jonas Rosendo <jonasrosendo1@gmail.com>
2023-11-29 08:44:12 -0300

    Adding main texts and main image

--- site/src/jsMain/kotlin/com/jonasrosendo/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/jonasrosendo/sections/MainSection.kt
@@ -4,22 +4,41 @@ import androidx.compose.runtime.Composable
 import com.jonasrosendo.components.Header
 import com.jonasrosendo.components.SocialBar
 import com.jonasrosendo.models.Section
+import com.jonasrosendo.models.Theme
+import com.jonasrosendo.styles.HireMeButtonStyle
+import com.jonasrosendo.styles.MainImageStyle
+import com.jonasrosendo.utils.Constants.FONT_FAMILY
+import com.jonasrosendo.utils.Constants.LOREM_IPSUM_SHORTEST
 import com.jonasrosendo.utils.Constants.SECTION_WIDTH
 import com.jonasrosendo.utils.Res
-import com.varabyte.kobweb.compose.css.ObjectFit
+import com.jonasrosendo.utils.Res.Image.MAIN_IMAGE
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.id
-import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun MainSection() {
+
+    val breakpoint = rememberBreakpoint()
     Box(
         modifier = Modifier
             .id(Section.Home.id)
@@ -27,7 +46,7 @@ fun MainSection() {
         contentAlignment = Alignment.TopCenter
     ) {
         MainBackground()
-        MainContent()
+        MainContent(breakpoint = breakpoint)
     }
 }
 
@@ -41,13 +60,129 @@ fun MainBackground() {
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(breakpoint: Breakpoint) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Header()
-        SocialBar()
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SimpleGrid(
+                modifier = Modifier
+                    .fillMaxWidth(
+                        if (breakpoint >= Breakpoint.MD) 80.percent else 90.percent
+                    ),
+                numColumns = numColumns(base = 1, md = 2)
+            ) {
+                MainText(breakpoint = breakpoint)
+                MainImage()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainText(breakpoint: Breakpoint) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (breakpoint > Breakpoint.MD) {
+            SocialBar()
+        }
+
+        Column {
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if(breakpoint >= Breakpoint.LG) 45.px else 20.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Primary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Hello, I'm")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(top = 20.px, bottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if (breakpoint >= Breakpoint.LG) 68.px else 40.px)
+                    .fontWeight(FontWeight.Bolder)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Jonathan Smith")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(top = 10.px, bottom = 5.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(20.px)
+                    .fontWeight(FontWeight.Bold)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("I'm mobile & Web Developer")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(bottom = 25.px)
+                    .maxWidth(400.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(15.px)
+                    .fontStyle(FontStyle.Italic)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(LOREM_IPSUM_SHORTEST)
+            }
+
+            Button(
+                attrs = HireMeButtonStyle.toModifier()
+                    .height(40.px)
+                    .border(width = 0.px)
+                    .borderRadius(r = 5.px)
+                    .background(Theme.Primary.rgb)
+                    .color(Colors.White)
+                    .cursor(Cursor.Pointer)
+                    .toAttrs()
+            ) {
+                Link(
+                    modifier = Modifier
+                        .color(color = Colors.White)
+                        .textDecorationLine(TextDecorationLine.None),
+                    text = "Hire Me!",
+                    path = Section.Contact.path
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainImage() {
+    Column(
+        modifier = MainImageStyle.toModifier()
+            .fillMaxSize(80.percent)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Image(
+            src = MAIN_IMAGE,
+            description = "Main Image",
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
--- site/src/jsMain/kotlin/com/jonasrosendo/utils/Constants.kt
+++ site/src/jsMain/kotlin/com/jonasrosendo/utils/Constants.kt
@@ -3,11 +3,13 @@ package com.jonasrosendo.utils
 object Constants {
     const val SECTION_WIDTH = 1920
     const val FONT_FAMILY = "roboto"
+    const val LOREM_IPSUM_SHORTEST = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 }
 
 object Res {
     object Image {
         const val BACKGROUND = "background.svg"
         const val LOGO = "logo.svg"
+        const val MAIN_IMAGE = "main_image.png"
     }
 }
